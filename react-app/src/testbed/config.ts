a6ece774a0ee9159843140ef7aa2c163c517ba32
Luke 10X <spam@luke10x.dev>
2023-03-23 20:21:52 -0400

    🔧 Configuration set to non-https becuase dropped using CRA

--- react-app/src/testbed/config.ts
+++ react-app/src/testbed/config.ts
@@ -1,7 +1,7 @@
 const OIDC_AUTH = 'http://localhost:28080/realms/bigrealm'
 
 const authenticateEndpoint = `${OIDC_AUTH}/protocol/openid-connect/auth`
-const redirectUri = "https://localhost:13000"
+const redirectUri = "http://localhost:13000"
 const tokenEndpoint = `${OIDC_AUTH}/protocol/openid-connect/token`;
 const restrictedBackendEndpoint = 'http://localhost:18080/secret'
 const backendUrlBase = 'http://localhost:18080'
